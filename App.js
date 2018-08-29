2292f6a9dcd3f532973f0526085a7f124272d552
jeo19 <terrakkk@outlook.com>
2018-08-29 21:29:22 +0630

    Styling the ToDo Component adn Add ToDo.js

--- App.js
+++ App.js
@@ -1,23 +1,89 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  StatusBar,
+  Dimensions,
+  Platform,
+  ScrollView
+} from "react-native";
+import ToDo from "./ToDo";
+const { height, width } = Dimensions.get("window");
 export default class App extends React.Component {
+  state = {
+    nowToDo: ""
+  };
   render() {
+    const { nowToDo } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>kawai todo</Text>
+        <View style={styles.card}>
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={nowToDo}
+            onChangeText={this._controllNewTodo}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
     );
   }
+  _controllNewTodo = text => {
+    this.setState({
+      nowToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F23657",
+    alignItems: "center"
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "400",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb",
+    fontSize: 20
+  },
+  toDos: {
+    alignItems: "center"
+  }
 });
